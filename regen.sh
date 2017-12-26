894a0e31a533bef20140c023958d9340084d2482
Eric Warnke <ericew@gmail.com>
2017-12-26 15:58:51 -0500

    Generate ads.conf directly

--- regen.sh
+++ regen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Fetch list
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-# Convert to unbound format
+wget -qO- https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
 grep "^0\.0\.0\.0" hosts | awk '{print "local-zone:", $2 , " refuse"}' > ads.conf
 
